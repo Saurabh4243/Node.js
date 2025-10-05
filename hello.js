b00a8c165d05f082cadc1ff5efa0a8853db5fa88
Saurabh Kothule <Saurabhkothule800@gmail.com>
2025-10-05 12:43:11 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,16 @@
+// const http=require('http');
+const express=require('express');
+
+const app=express();
+
+app.get("/",(req,res)=>{
+    return res.send("hello from home page");
+});
+app.get("/about",(req,res)=>{
+    return res.send("hello from about page" +"hey" +req.query.name + "you are "+req.query.age +"old");
+});
+app.listen(8000,()=>{
+    console.log("server started");
+})
+// const myServer=http.createServer(app);
+// myServer.listen(8000,()=>console.log("server started"));
